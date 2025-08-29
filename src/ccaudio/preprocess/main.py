9524654980a0db6e1d50d9b721bc40a0c2d30bc5
Kohei ASAI <ko2001he@gmail.com>
2025-08-29 12:00:35 +0900

    map cutset

--- src/ccaudio/preprocess/main.py
+++ src/ccaudio/preprocess/main.py
@@ -3,12 +3,16 @@ from pathlib import Path
 
 from lhotse import CutSet
 
+from ccaudio.preprocess.pipeline.convert_audio import convert_audio
+
 
 def main(shar_dir: Path) -> None:
     cut_paths = sorted(list(map(str, shar_dir.glob("**/cuts.*.jsonl.gz"))))
     recording_paths = sorted(list(map(str, shar_dir.glob("**/recording.*.tar"))))
 
     cuts = CutSet.from_shar({"cuts": cut_paths, "recording": recording_paths})
+
+    cuts = cuts.map(convert_audio)
 
     for cut in cuts.data:
         print(cut)
