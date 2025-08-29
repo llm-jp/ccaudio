9a2ccc1369369409022327c817d6511fd13eb7bb
Kohei ASAI <ko2001he@gmail.com>
2025-08-29 13:41:39 +0900

    add whisper detect lang

---
+++ src/ccaudio/preprocess/pipeline/whisper_detect_lang.py
@@ -0,0 +1,26 @@
+import numpy as np
+from faster_whisper import WhisperModel
+from lhotse import MonoCut
+
+
+def whisper_detect_lang(cut: MonoCut, model: WhisperModel) -> MonoCut:
+    audio = cut.load_audio()
+    assert isinstance(audio, np.ndarray)
+
+    features = model.feature_extractor(audio, chunk_length=30)
+
+    lang, lang_prob, _ = model.detect_language(
+        features=features,
+        vad_filter=True,
+        language_detection_segments=7,
+        language_detection_threshold=0.5,
+    )
+
+    for s in cut.supervisions:
+        s.language = lang
+        if s.custom is not None:
+            s.custom["lang_prob"] = lang_prob
+        else:
+            s.custom = {"lang_prob": lang_prob}
+
+    return cut
---
+++ tests/preprocess/pipeline/test_whisper_detect_lang.py
@@ -0,0 +1,42 @@
+import io
+
+import numpy as np
+import soundfile as sf
+from faster_whisper import WhisperModel
+from lhotse import MonoCut, Recording
+from lhotse.cut.mono import SupervisionSegment
+
+from ccaudio.preprocess.pipeline.whisper_detect_lang import whisper_detect_lang
+
+
+def test_whisper_detect_lang() -> None:
+    wav = np.random.randn(16000)
+    sr = 16000
+
+    buf = io.BytesIO()
+    sf.write(buf, wav.T, sr, format="WAV")
+
+    recording = Recording.from_bytes(buf.getvalue(), recording_id="recording_id")
+    assert recording.channel_ids is not None
+
+    cut = MonoCut(
+        id="id",
+        start=0,
+        duration=recording.duration,
+        channel=0,
+        recording=recording,
+        supervisions=[
+            SupervisionSegment(
+                id="segment_id",
+                recording_id=recording.id,
+                start=0,
+                duration=recording.duration,
+                channel=0,
+            )
+        ],
+    )
+
+    model = WhisperModel(model_size_or_path="large-v3")
+
+    cut = whisper_detect_lang(cut, model)
+    assert isinstance(cut, MonoCut)
